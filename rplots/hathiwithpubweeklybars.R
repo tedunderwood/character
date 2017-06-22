df6311566876397a3921e9522e91db5586bc0e9d
tedunderwood <tunder@illinois.edu>
2017-06-22 07:42:50 -0500

    add R code for pubweekly errorbars

---
+++ rplots/hathiwithpubweeklybars.R
@@ -0,0 +1,8 @@
+errors <- read.csv('/Users/tunder/Dropbox/python/character/dataforR/pubweeklyerrorbars.csv')
+hathi <- read.csv('/Users/tunder/Dropbox/python/character/dataforR/authorratios.csv')
+p <- ggplot() + 
+  geom_errorbar(data=errors, mapping=aes(x=year, ymin=low, ymax=high), width=5, size=1, color="blue") + 
+  geom_point(data=errors, mapping=aes(x=year, y=mean), size=4, shape=21, fill="white") +
+  geom_line(data = hathi, mapping = aes(x = year, y = authratio)) + 
+  xlim(1850, 2007) + ylim(0, 0.65)
+plot(p)
