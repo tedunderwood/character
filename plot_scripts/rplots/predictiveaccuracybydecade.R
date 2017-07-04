b5a06b511df9421a8f5e868690809f8506b489b4
tunder <tunder@lism01-tunder.clinton>
2017-07-04 17:03:23 -0400

    cosmetic changes to viz

--- plot_scripts/rplots/predictiveaccuracybydecade.R
+++ plot_scripts/rplots/predictiveaccuracybydecade.R
@@ -5,8 +5,9 @@ library(ggplot2)
 library(scales)
 
 p <- ggplot(d2, aes(x = decade, y = accuracy)) + 
-  geom_point(color = 'black', alpha = 0.7, shape = 21, fill = 'blue2', size = 2) +
+  geom_point(color = 'black', alpha = 0.6, shape = 21, fill = 'blue2', size = 2) +
   theme(text = element_text(size = 14)) +
+  theme(plot.title = element_text(hjust = 0.5)) +
   scale_y_continuous('predictive accuracy\n', labels = percent) +
   xlab('') + ggtitle('Accuracy of gender prediction, 1600-character samples\n')
 plot(p)
