a59ad5049484b5cf081f75aef8197d4eea032d79
tunder <tunder@lism01-tunder.fios-router.home>
2017-07-09 12:31:37 -0400

    viz scripts for figs 10-14

---
+++ plot_scripts/rplots/Fig12roomchamberhousecountry.R
@@ -0,0 +1,18 @@
+library(ggplot2)
+library(dplyr)
+library(reshape2)
+
+df <- read.csv('/Users/tunder/Dropbox/python/character/dataforR/diff_matrix.csv')
+df <- df[df$thedate > 1799, ]
+
+subset <- select(df, thedate, room, chamber, house, country)
+
+longform <- melt(subset, id.vars = c('thedate'))
+
+p <- ggplot(longform, aes(x = thedate, y = value, color = variable, shape = variable)) + geom_point() + geom_smooth(span = 0.6, se = FALSE) +
+  scale_color_manual(name = "word\n", values = c('gray40', 'red2', 'dodgerblue', 'olivedrab')) +
+  theme(text = element_text(size = 16)) + scale_shape(name="word\n") +
+  scale_y_continuous('Uses for women, minus uses for men,\n in 10k words selected equally from both\n') + 
+  scale_x_continuous("", breaks = c(1800,1850,1900,1950,2000))
+
+plot(p)
