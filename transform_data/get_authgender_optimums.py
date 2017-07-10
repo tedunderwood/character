02d3ed7bfd5ad76087f64a49f71b4f39c81eaff2
tunder <tunder@illinois.edu>
2017-07-09 22:09:39 -0400

    scripts for tuning hyperparameters

--- transform_data/get_authgender_optimums.py
+++ transform_data/get_authgender_optimums.py
@@ -7,10 +7,10 @@ import csv
 maximums = dict()
 L2s = dict()
 
-with open('../dataforR/authgender_predictions.tsv', encoding = 'utf-8') as f:
+with open('../dataforR/auth_specific_charpredict.tsv', encoding = 'utf-8') as f:
     reader = csv.DictReader(f, delimiter = '\t')
     for row in reader:
-        key = (row['meandate'], row['iter'])
+        key = (row['decade'], row['iter'], row['authgender'])
         accuracy = float(row['accuracy'])
         if key not in maximums:
             maximums[key] = accuracy
@@ -20,12 +20,13 @@ with open('../dataforR/authgender_predictions.tsv', encoding = 'utf-8') as f:
             L2s[key] = row['L2']
 
 
-with open('../dataforR/authgender_prediction_optimums.tsv', mode = 'w', encoding = 'utf-8') as f:
-    f.write('decade\tL2\taccuracy\n')
+with open('../dataforR/auth_specific_optimums.tsv', mode = 'w', encoding = 'utf-8') as f:
+    f.write('decade\tauthgender\tL2\taccuracy\n')
     for key, acc in maximums.items():
         decade = key[0]
+        authgender = key[2]
         L2 = L2s[key]
-        f.write(decade + '\t' + L2 + '\t' + str(acc) + '\n')
+        f.write(decade + '\t' + authgender + '\t' + L2 + '\t' + str(acc) + '\n')
 
 
 
