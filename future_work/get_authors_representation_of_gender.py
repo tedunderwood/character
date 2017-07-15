9f893e26df889e95ba67feb81fdd0361dc652496
tedunderwood <tunder@illinois.edu>
2017-07-15 16:07:31 -0500

    bug fixes

--- future_work/get_authors_representation_of_gender.py
+++ future_work/get_authors_representation_of_gender.py
@@ -1,3 +1,32 @@
+#!/usr/bin/env python3
+
+# This script is designed to apply a particular model of
+# gender to characters in a particular span of time,
+# and record the predicted probabilities.
+
+# It summarizes and records the predicted probabilities
+# both at the author level and at the "story" (document)
+# level. In this dataset, some 19c stories include more
+# than one volume.
+
+# USAGE:
+
+# python3 get_authors_representation_of_gender.py source.csv genderprobs.tsv outdir
+
+# source.csv       a csv like fiction_prestige_results.csv
+# genderprobs.tsv  a probability file created by apply_model_to_characters.py
+#                  in my data th is called character_probabilities.tsv
+# outdir           a folder, in which storymeta.tsv and authormeta.tsv files
+#                  will be created
+
+# DEPENDENCIES:
+# it expects SonicScrewdriver.py and aliasfile.tsv to be in the
+# same directory.
+
+# it also expects an enlarged version of filtered_fiction_metadata
+# (one that includes vols 1780-1799) to be in a sibling "metadata"
+# directory, as it is in the repo
+
 import csv, os, sys
 import pandas as pd
 import numpy as np
@@ -5,34 +34,70 @@ import numpy as np
 from collections import Counter
 
 # import utils
-currentdir = os.path.dirname(__file__)
-libpath = os.path.join(currentdir, '../../lib')
-sys.path.append(libpath)
+# currentdir = os.path.dirname(__file__)
+# sys.path.append(currentdir)
 
 import SonicScrewdriver as utils
 
-prestige = pd.read_csv('/Users/tunder/Dropbox/GenreProject/python/reception/fiction/prestigery.csv')
-metadata = pd.read_csv('/Users/tunder/Dropbox/python/character/metadata/filtered_fiction_plus_18c.tsv', sep ='\t')
+# UNPACK ARGUMENTS
+
+args = sys.argv
+if len(args) < 4:
+    print('This script expects three arguments.')
+    print('See usage instructions in the code.')
+    sys.exit(0)
+
+prestige_source_path = args[1]
+gender_prob_path = args[2]
+outdir = args[3]
+
+# LOAD DATA
+
+prestige = pd.read_csv(prestige_source_path)
+metadata = pd.read_csv('../metadata/filtered_fiction_plus_18c.tsv', sep ='\t')
 
 metadata['volid'] = metadata.volid.apply(utils.clean_pairtree)
 
-# Let's start by identifying all stories in our metadata by
-# authors in the prestige dataset. We're only going to consider
-# the first 25 chars of authname
+# Read in aliasfile:
+aliases = dict()
+with open('aliasfile.tsv', encoding = 'utf-8') as f:
+    reader = csv.DictReader(f, delimiter = '\t')
+    for row in reader:
+        aliases[row['alias']] = row['ourname']
 
-def trim_to_25(aname):
+# Let's start by identifying all stories in our metadata by
+# authors in the prestige dataset.
+
+# Author names can vary. So first, we're going to apply
+# a translation function that converts various aliases
+# into "ourname" -- a name present in the metadata file.
+
+# then we strip punctuation, trim to 25 chars, kill periods
+# after initials, and lowercase, to increase matches.
+
+def translate_aliases(aname):
+    global aliases
+    if aname in aliases:
+        return aliases[aname]
+    else:
+        return aname
+
+def trim_to_24(aname):
     if type(aname) != str:
         return 'Anonymous'
 
-    aname = aname.strip('(),. .{0123456789]').lower()
+    aname = aname.strip('(),. .[0123456789]').lower()
+    aname = aname.replace('.', '')
     if len (aname) > 24:
         return aname[0:24]
     else:
         return aname
 
-known_authors = set([trim_to_25(a) for a in prestige.author])
-prestige['trimmedauth'] = prestige.author.apply(trim_to_25)
-metadata['trimmedauth'] = metadata.author.apply(trim_to_25)
+known_authors = set([trim_to_24(a) for a in prestige.author])
+prestige['trimmedauth'] = prestige.author.apply(trim_to_24)
+
+metadata['trimmedauth'] = metadata.author.apply(translate_aliases)
+metadata['trimmedauth'] = metadata.trimmedauth.apply(trim_to_24)
 all_matches = set(metadata.trimmedauth)
 
 auth2story = dict()
@@ -54,7 +119,7 @@ for a in known_authors:
 
 # now let's get all the characters for each story
 
-data = pd.read_csv('/Users/tunder/Dropbox/python/character/data/character_probabilities.tsv', sep = '\t')
+data = pd.read_csv(gender_prob_path, sep = '\t')
 #loads characters
 
 storydates = dict()
@@ -173,13 +238,15 @@ authorcolumns = ['author', 'num_stories', 'authgender', 'meandate', 'mean_presti
 
 storycolumns = ['docid', 'author', 'authgender', 'pubdate', 'numchars', 'charsize', 'pct_women', 'wordratio','prob_diff', 'prob_stdev', 'prob_mean']
 
-with open('storymeta.tsv', mode = 'w', encoding = 'utf-8') as f:
+outpath = os.path.join(outdir, 'storymeta.tsv')
+with open(outpath, mode = 'w', encoding = 'utf-8') as f:
     writer = csv.DictWriter(f, delimiter = '\t', fieldnames = storycolumns)
     writer.writeheader()
     for s in storyout:
         writer.writerow(s)
 
-with open('authormeta.tsv', mode = 'w', encoding = 'utf-8') as f:
+outpath = os.path.join(outdir, 'authormeta.tsv')
+with open(outpath, mode = 'w', encoding = 'utf-8') as f:
     writer = csv.DictWriter(f, delimiter = '\t', fieldnames = authorcolumns)
     writer.writeheader()
     for a in authorout:
